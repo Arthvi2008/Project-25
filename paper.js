99b6ef05b15b63a73a225d867ef109547d897ef6
Arthvi2008 <78147538+Arthvi2008@users.noreply.github.com>
2021-04-27 23:35:44 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,14 @@
+var paper;
+var options={
+    isstatic:false,
+    restitution:0.3,
+    friction:0,
+    density:1.2,
+
+}
+
+function keyPressed() {
+    if (keycode --- UP_ARROW) (
+    Matter.Body.applyForce(paperObject.body, paperObject.body.position, (x:130,y:-145));
+     }
+    }
